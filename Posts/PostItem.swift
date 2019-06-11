daa82b635a64b006ddf423eac7a3a7e64cc814cf
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2019-06-11 22:48:58 +0300

    Create the PostItem model

---
+++ Posts/PostItem.swift
@@ -0,0 +1,26 @@
+//
+//  PostItem.swift
+//  Posts
+//
+//  Created by Pusca Ghenadie on 11/06/2019.
+//  Copyright © 2019 Pusca Ghenadie. All rights reserved.
+//
+
+import Foundation
+
+public struct PostItem {
+    public let id: Int
+    public let userId: Int
+    public let title: String
+    public let body: String
+    
+    public init(id: Int,
+                userId: Int,
+                title: String,
+                body: String) {
+        self.id = id
+        self.title = title
+        self.userId = userId
+        self.body = body
+    }
+}
