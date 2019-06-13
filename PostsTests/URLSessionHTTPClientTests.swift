7fdcca37c395efa0d24437f0a458230c99a52f97
Pusca, Ghenadie <ghenadie.pusca@garmin.com>
2019-06-13 13:50:01 +0300

    Add URLProtocol stub

--- PostsTests/URLSessionHTTPClientTests.swift
+++ PostsTests/URLSessionHTTPClientTests.swift
@@ -10,4 +10,31 @@ import XCTest
 
 class URLSessionHTTPClientTests: XCTestCase {
 
+    private class URLProtocolStub: URLProtocol {
+        private static var stubs = [URL: Stub]()
+
+        private struct Stub {
+            let error: Error?
+        }
+
+        override class func canInit(with request: URLRequest) -> Bool {
+            return true
+        }
+
+        override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+            return request
+        }
+
+        override func startLoading() {
+            guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
+
+            if let error = stub.error {
+                client?.urlProtocol(self, didFailWithError: error)
+            }
+
+            client?.urlProtocolDidFinishLoading(self)
+        }
+
+        override func stopLoading() {}
+    }
 }
