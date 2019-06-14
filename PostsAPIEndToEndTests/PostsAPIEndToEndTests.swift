0a2787054421854c2f37f56eb3b87ef1a437091f
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2019-06-14 19:28:20 +0300

    End to end test api

--- PostsAPIEndToEndTests/PostsAPIEndToEndTests.swift
+++ PostsAPIEndToEndTests/PostsAPIEndToEndTests.swift
@@ -12,4 +12,46 @@ import RxSwift
 
 class PostsAPIEndToEndTests: XCTestCase {
     
+    func test_getDataFromTestServerGetsFixedTestData() {
+        let client = URLSessionHTTPClient()
+        let testURL = URL(string: "https://poststestapi.free.beeceptor.com/")!
+        let loader = RemotePostsLoader(url: testURL, client: client)
+        
+        let exp = expectation(description: "Wait for load")
+
+        let expectedPosts = expectedFixedPostItems()
+        loader.load().subscribe { result in
+            switch result {
+            case .success(let receivedPosts):
+                XCTAssertEqual(receivedPosts[0], expectedPosts[0])
+                XCTAssertEqual(receivedPosts[1], expectedPosts[1])
+                XCTAssertEqual(receivedPosts[2], expectedPosts[2])
+            default:
+                XCTFail("Expected to get posts, got \(result)")
+            }
+            
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 5.0)
+    }
+    
+    private func expectedFixedPostItems() -> [PostItem] {
+        let item1 = PostItem(id: 1,
+                             userId: 1,
+                             title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                             body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto")
+        
+        let item2 = PostItem(id: 2,
+                             userId: 2,
+                             title: "qui est esse",
+                             body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla")
+        
+        let item3 = PostItem(id: 3,
+                             userId: 3,
+                             title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+                             body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut")
+        
+        return [item1, item2, item3]
+    }
 }
