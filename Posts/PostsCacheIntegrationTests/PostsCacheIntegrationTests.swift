ffb2ffb1e01dd55d0f39fde75fd4ebd43a4eb5b9
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2019-06-16 10:16:22 +0300

    Add CacheIntegrationTests target

---
+++ Posts/PostsCacheIntegrationTests/PostsCacheIntegrationTests.swift
@@ -0,0 +1,15 @@
+//
+//  PostsCacheIntegrationTests.swift
+//  PostsCacheIntegrationTests
+//
+//  Created by Pusca Ghenadie on 16/06/2019.
+//  Copyright © 2019 Pusca Ghenadie. All rights reserved.
+//
+
+import XCTest
+import Posts
+import RxSwift
+
+class PostsCacheIntegrationTests: XCTestCase {
+
+}
