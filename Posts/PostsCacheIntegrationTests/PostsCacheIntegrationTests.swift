9e052cf77bbd33b3e294acf02840c761c0a6e45c
Pusca Ghenadie <pusca.ghenadie@gmail.com>
2019-06-16 10:20:43 +0300

    Setup CacheIntegrationTests utils

--- Posts/PostsCacheIntegrationTests/PostsCacheIntegrationTests.swift
+++ Posts/PostsCacheIntegrationTests/PostsCacheIntegrationTests.swift
@@ -11,5 +11,35 @@ import Posts
 import RxSwift
 
 class PostsCacheIntegrationTests: XCTestCase {
-
+    
+    
+    let disposeBag = DisposeBag()
+    
+    override func setUp() {
+        super.setUp()
+        
+        setEmptyCache()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        removeCachedItems()
+    }
+    
+    private func setEmptyCache() {
+        deleteCached()
+    }
+    
+    private func removeCachedItems() {
+        deleteCached()
+    }
+    
+    private func deleteCached() {
+        try? FileManager.default.removeItem(at: testStoreURL())
+    }
+    
+    private func testStoreURL() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appendingPathComponent("\(type(of: self)).store")
+    }
 }
